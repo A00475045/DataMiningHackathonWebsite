557778e27c27a6d9eddabbab0ff513e5baa99ac9
Ranjit Pandey A00475045 <ranjit.parshuram.pandey@smu.ca>
2024-03-11 01:56:18 -0300

    adds multiple dummy location

--- hackacthon-frontend/src/App.js
+++ hackacthon-frontend/src/App.js
@@ -1,43 +1,56 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import obj from './asset/hardcorded';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicmFuaml0dHR0dCIsImEiOiJjbHRreDZqNm0xOGc2MnJwYTVxMG5qcTBzIn0.zY3DpiT5B_8bYwGTz7iFIA';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmFuaml0dHR0dCIsImEiOiJjbHRreDZqNm0xOGc2MnJwYTVxMG5qcTBzIn0.zY3DpiT5B_8bYwGTz7iFIA";
 
-const Marker = ({ onClick, children, feature }) => {
-  const [mouseOnPoint, setMouseOnPoint] = useState(false);
-  const _onClick = () => {
-    // onClick(feature);
-    setMouseOnPoint(true)
-    // console.log(feature.Title);
-  };
-  const amenitiesArray = Object.keys(feature.Amenities).map(key => (
-    { name: key, value: feature.Amenities[key] }
-  ));
+const Marker = ({
+  isPropertyClicked,
+  setIsPropertyClicked,
+  children,
+  feature,
+}) => {
+  const _onClick = () => setIsPropertyClicked(feature);
+  const amenitiesArray = [] || Object.keys(feature.Amenities).map((key) => ({
+    name: key,
+    value: feature.Amenities[key],
+  }));
 
-  return (<>
-    {/* onMouseLeave={() => setMouseOnPoint(false) }*/}
-    {/* onMouseLeave={() => setTimeout(() => setMouseOnPoint(false), [2000])} */}
-    {mouseOnPoint && <div className='cancel-container' onClick={() => setMouseOnPoint(false)}><p className='cancel'>&#10060;</p></div>}
-    {mouseOnPoint && <div className='popup'  >
-      <div className='tile-header'><span className='tile-header-item title'>{feature.Title}</span>
-        <span className='tile-header-item'>CAD {feature.Price}</span>
+  return (
+    <>
+      {console.log("->", isPropertyClicked)}
+      {isPropertyClicked && (isPropertyClicked.properties.BUILDING_ID === feature.properties.BUILDING_ID) && (
+        <div
+          className="cancel-container"
+          onClick={() => setIsPropertyClicked(null)}
+        >
+          <p className="cancel">&#10060;</p>
+        </div>
+      )}
+      {isPropertyClicked && isPropertyClicked.properties.BUILDING_ID === feature.properties.BUILDING_ID && (
+        <div className="popup" >
+          <div className="tile-header">
+            <span className="tile-header-item title">{feature.properties.BUILDING_ID}</span>
+            <span className="tile-header-item">CAD {feature.properties.BUILDING_ID}</span>
+          </div>
+          <div className="tile-content">
 
-      </div>
-      <div className='tile-content'>
-        {/* {console.log(amenitiesArray)} */}
-
-        {/* { */}
-        {/* // Object.keys(feature.Amenities).map(key =>  */}
-        {/* // return (<div>{amen} {amen ? "&#10004;" : "&#10060;"}</div>))} */}
-        {amenitiesArray.map((amen) => (<div className='amenities-tile'><p>{amen.name}</p> <p>{amen.value ? '\u2705' : '\u274C'}</p></div>))}
-      </div>
-
-    </div>}
-    {!mouseOnPoint && <button onClick={_onClick} className="marker">
-      {children}
-    </button>}
-  </>
+            {/*amenitiesArray.map((amen) => (
+              <div className="amenities-tile">
+                <p>{amen.name}</p> <p>{amen.value ? "\u2705" : "\u274C"}</p>
+              </div>
+            ))*/}
+          </div>
+        </div>
+      )}
+      {!(isPropertyClicked != null) && (
+        <button onClick={_onClick} className="marker" titlea={feature.properties.BUILDING_ID}>
+          {children}
+        </button>
+      )}
+    </>
   );
 };
 
@@ -47,50 +60,24 @@ export default function App() {
   const [lng, setLng] = useState(-63.6098533);
   const [lat, setLat] = useState(44.6532031);
   const [zoom, setZoom] = useState(14.24);
+  const [isPropertyClicked, setIsPropertyClicked] = useState(null);
 
 
-  const obj = [
-    {
-      "Title": "2 Bedroom Apartment for Rent - 211, 221 Glenforest Drive",
-      "Price": 300,
-      "Location": "221 Glenforest Drive, Halifax, NS, B3M 1J3",
-
-      "lat": 44.6537,
-      "lon": -63.6096,
-      "Amenities": {
-        "pool": true,
-        "parking": false,
-        "utilities-included": true
-      }
-    }, {
-      "Title": "B",
-      "Price": 200,
-      "Location": "221 Glenforest Drive, Halifax, NS, B3M 1J3",
-      "lat": 44.6520,
-      "lon": -63.6119,
-      "Amenities": {
-        "pool": false,
-        "parking": false,
-        "utilities-included": true
-      }
-    },
-  ]
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    // if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
+      style: "mapbox://styles/mapbox/streets-v12",
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
     });
 
-    map.current.on('move', () => {
+    map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
-
 
     obj.map((o) => {
       const ref = React.createRef();
@@ -98,19 +85,20 @@ export default function App() {
       ref.current = document.createElement("div");
       // Render a Marker Component on our new DOM node
       ReactDOM.render(
-        <Marker feature={o} >
-
-        </Marker>,
-        ref.current
+        <Marker
+          feature={o}
+          isPropertyClicked={isPropertyClicked}
+          setIsPropertyClicked={setIsPropertyClicked}
+        ></Marker>,
+        ref.current,
       );
 
-
-      return (new mapboxgl.Marker(ref.current).setLngLat([o.lon, o.lat]).addTo(map.current))
-    }
-    )
-
-
-  });
+      // .setLngLat([-63.6098533, 44.6532031])
+      return new mapboxgl.Marker(ref.current)
+        .setLngLat([o.properties.LON, o.properties.LAT])
+        .addTo(map.current);
+    });
+  }, [isPropertyClicked]);
 
   return (
     <div>
